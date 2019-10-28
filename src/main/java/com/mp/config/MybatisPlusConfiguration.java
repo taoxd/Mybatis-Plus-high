7f30c15eb174718823e2402981a41224e9dd672a
taoxd <250567492@qq.com>
2019-10-28 23:43:28 +0800

    性能分析实现及参数设置

--- src/main/java/com/mp/config/MybatisPlusConfiguration.java
+++ src/main/java/com/mp/config/MybatisPlusConfiguration.java
@@ -1,8 +1,10 @@
 package com.mp.config;
 
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MybatisPlusConfiguration {
@@ -18,6 +20,20 @@ public class MybatisPlusConfiguration {
     @Bean
     public OptimisticLockerInterceptor optimisticLockerInterceptor() {
         return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * SQL执行效率插件
+     */
+    @Bean
+    @Profile({"dev", "test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        //格式化打印sql
+        performanceInterceptor.setFormat(true);
+        //设值sql最大执行时间，超过就报错
+        performanceInterceptor.setMaxTime(5L);
+        return performanceInterceptor;
     }
 }
 
